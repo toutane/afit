ac453cbd78343616f82375133b97462325487359
Charles-Antoine Leger <charles-antoine.leger@epita.fr>
2021-11-19 22:50:05 +0100

    Passed tests for basic_arithmetics.ml

--- Source/builtin/basic_arithmetics.ml
+++ Source/builtin/basic_arithmetics.ml
@@ -6,7 +6,10 @@ open Builtin
     @param a non-zero integer
     @param b non-zero integer
 *)
-let rec gcd a b = 0 ;;
+let rec gcd a b =
+  if b = 0 then a
+  else gcd b (modulo a b)
+;;
 
 (** Extended euclidean division of two integers NOT OCAML DEFAULT.
     Given non-zero entries a b computes triple (u, v, d) such that
@@ -14,4 +17,10 @@ let rec gcd a b = 0 ;;
     @param a non-zero integer
     @param b non-zero integer.
 *)
-let bezout a b = (0,0,0) ;;
+let bezout a b =
+  let rec loop r1 u1 v1 r2 u2 v2 =
+    if r2 = 0 then (u1, v1, r1)
+    else let q = quot r1 r2
+         in loop r2 u2 v2 (r1 - q * r2) (u1 - q * u2) (v1 - q * v2)
+  in loop a 1 0 b 0 1
+;;
