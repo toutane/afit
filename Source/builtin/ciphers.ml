854a1d30931c28c3e499ac7098f3ab1ee441105a
Charles-Antoine Leger <charles-antoine.leger@epita.fr>
2021-11-28 10:36:39 +0100

    Do Caesar cipher and rsa functions in ciphers.ml

--- Source/builtin/ciphers.ml
+++ Source/builtin/ciphers.ml
@@ -1,4 +1,4 @@
-(** Ciphers
+ (** Ciphers
     Builtin integer based ciphers.
 *)
 
@@ -13,15 +13,24 @@ open Power
     @param m word to cipher.
     @param b base ; for ASCII codes should be set to 256.
  *)
-let encrypt_cesar k m b = []
-
+let encrypt_cesar k m b =
+  let rec browse_list = function
+      [] -> []
+    | num::l -> (modulo (num + k) b)::(browse_list l)
+  in browse_list m
+;;
+  
 (** Cesar's cipher decryption
     @param k is an integer corresponding to key
     @param m encrypted word.
     @param b base ; for ASCII code should be set to 256.
  *)
-let decrypt_cesar k m b = []
-
+let decrypt_cesar k m b =
+  let rec browse_list = function
+      [] -> []
+    | num::l -> (modulo (num - k) b)::(browse_list l)
+  in browse_list m
+;;
 
 (********** RSA Cipher **********)
 
@@ -31,19 +40,44 @@ let decrypt_cesar k m b = []
     @param p prime number
     @param q prime number
 *)
-let generate_keys_rsa p q = ((0, 0), (0, 0))
+let generate_keys_rsa p q =
+  let n = p * q and phi = (p - 1) * (q - 1) and e = 3 in
+  if (gcd phi e) != 1 then (
+    let rec find_e x =
+      if (gcd phi x) == 1 then x
+      else find_e (x + 2)
+    in let new_e = find_e (e + 2)
+       in let (d, _, _) = bezout new_e phi
+          in ((n, new_e), (n, d))
+  )
+  else 
+    let (d, _, _) = bezout e phi
+    in ((n, e), (n, d))
+   
+;;
+
+(*
+generate_keys_rsa 9967 9973;;
+generate_keys_rsa 3 11;;
+gcd ((9967 - 1) * (9973 - 1)) 5;;  
+ *)
 
 (** Encryption using RSA cryptosystem.
     @param m integer hash of message
     @param pub_key a tuple (n, e) composing public key of RSA cryptosystem.
  *)
-let encrypt_rsa m (n, e) = 0
+let encrypt_rsa m (n, e) =
+  mod_power m e n
+;;
 
 (** Decryption using RSA cryptosystem.
     @param m integer hash of encrypter message.
     @param pub_key a tuple (n, d) composing private key of RSA cryptosystem.
  *)
-let decrypt_rsa m (n , d) = 0
+
+let decrypt_rsa m (n , d) =
+  mod_power m d n
+;;
 
 
 (********** ElGamal Cipher **********)
