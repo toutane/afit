96336237445c612fceddcd6109e4ec8eb46e7e31
Charles-Antoine Leger <charles-antoine.leger@epita.fr>
2021-11-28 10:35:44 +0100

    Debug mod_power function in power.ml

--- Source/builtin/power.ml
+++ Source/builtin/power.ml
@@ -32,13 +32,17 @@ let power x n =
     @param n exponent
     @param m modular base
  *)
+
 let mod_power x n m =
-  let rec multi_mf x i =
-    if i = 0 then 1
-    else let k = if modulo i 2 = 0 then multi_mf (x*x) (quot i 2)
-                 else x * multi_mf (x*x) (quot (i - 1) 2 )
-         in modulo k m
-  in multi_mf x n  
+  let rec multiply b e =
+    if e = 0 then 1
+    else 
+      if e = 1 then modulo b m
+      else let t = multiply b (e / 2) in
+           let k = modulo (t * t) m
+           in if (e mod 2) = 0 then k
+              else modulo (k * (modulo b m)) m
+  in multiply x n
 ;;
 
 (** Fast modular exponentiation function mod prime. Logarithmic complexity.
