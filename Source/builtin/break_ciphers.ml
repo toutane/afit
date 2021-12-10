e5836103f370fd6b87ef3637d4dbd8c6584c3e68
toutane <ca@leger.email>
2021-12-10 21:27:55 +0100

    Complete break_ciphers.ml

--- Source/builtin/break_ciphers.ml
+++ Source/builtin/break_ciphers.ml
@@ -6,4 +6,12 @@ open Basic_arithmetics
 (** Factors product of two primes.
     @param key is public key of an RSA cryptosystem.
  *)
-let break key = (0, 0)
+let break key =
+  let (n, e) = key in
+  let rec getP x =
+    if modulo n x = 0 then x
+    else getP (x - 1)
+  in let p = getP (n - 1) in
+     let q = n / p
+     in (q, p)
+;;
