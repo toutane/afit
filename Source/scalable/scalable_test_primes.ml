7a2c38a8f045771f976c8fa9c4692ca98e6f9596
toutane <ca@leger.email>
2021-12-15 23:04:54 +0100

    copy builtin tests for primes for scalable

--- Source/scalable/scalable_test_primes.ml
+++ Source/scalable/scalable_test_primes.ml
@@ -5,10 +5,31 @@ open Scalable_basic_arithmetics
 open Scalable_power
 
 (** Deterministic primality test *)
-let is_prime n = true
+let is_prime n =
+  if mod_b n [ 0; 0; 1 ] = [] then if n = [ 0; 0; 1 ] then true else false
+  else
+    let rec div_test i =
+      if compare_b i n >= 0 then true
+      else if mod_b n i = [] then false
+      else div_test (add_b i [ 0; 0; 1 ])
+    in
+    div_test [ 0; 1; 1 ]
 
 (** Pseudo-primality test based on Fermat's Little Theorem
     @param p tested bitarray
     @param testSeq sequence of bitarrays againt which to test
  *)
-let is_pseudo_prime p test_seq = true
+let is_pseudo_prime p test_seq =
+  if p = [ 0; 0; 1 ] then true (* a refaire... *)
+  else
+    let rec power_mod_div = function
+      | [] -> true
+      | e :: l ->
+          if e = p then power_mod_div l
+          else if
+            gcd_b p e = [ 0; 1 ]
+            && mod_b (prime_mod_power e (diff_b p [ 0; 1 ]) p) p = [ 0; 1 ]
+          then power_mod_div l
+          else false
+    in
+    power_mod_div test_seq
